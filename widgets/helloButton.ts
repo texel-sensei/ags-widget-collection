f461125293d7664f58e953ce9794c698fe21dab2
texel <texelsensei@posteo.net>
2024-09-15 21:44:56 +0200

    add missing type annotation to helloButton

--- widgets/helloButton.ts
+++ widgets/helloButton.ts
@@ -1,5 +1,6 @@
+import Gtk from "gi://Gtk?version=3.0";
 
-export function HelloButton(text: string) {
+export function HelloButton(text: string): Gtk.Widget {
 	return Widget.Button({
 		label: "Click Me",
 		on_clicked: () => Utils.notify(text),
